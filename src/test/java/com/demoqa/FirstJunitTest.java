51da139aa58be7aa9299dbf3f2730c81c94731c9
Juli.simon <juli.simonova@gmail.com>
2023-03-28 23:38:13 +0300

    junit example

--- src/test/java/com/demoqa/FirstJunitTest.java
+++ src/test/java/com/demoqa/FirstJunitTest.java
@@ -1,4 +1,27 @@
 package com.demoqa;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FirstJunitTest {
+
+
+    @BeforeEach
+    void beforeEach () {
+        System.out.println("      #### @BeforeEach");
+    }
+
+    @Test
+    void firstTest() {
+        Assertions.assertTrue(3 > 2);
+        System.out.println("             #### @Test firstTest()");
+    }
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(3 > 2);
+        System.out.println("             #### @Test secondTest()");
+    }
+
 }
